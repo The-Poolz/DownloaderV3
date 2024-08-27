3b3b8fc8515e8ac83df8e69f722507d682f60ca5
Artem Krol <artemkrol47@gmail.com>
2024-08-27 06:14:26 -0500

    Refactor ERC20CacheManager

--- src/DownloaderV2/Helpers/ERC20CacheManager.cs
+++ src/DownloaderV2/Helpers/ERC20CacheManager.cs
@@ -1,35 +1,17 @@
-﻿using Amazon;
-using Amazon.DynamoDBv2;
-using DownloaderV2.Decoders;
-using DownloaderV2.Utilities;
+﻿using DownloaderV2.Utilities;
 using Net.Cache.DynamoDb.ERC20;
-using Amazon.DynamoDBv2.DataModel;
 using Net.Cache.DynamoDb.ERC20.Models;
 
 namespace DownloaderV2.Helpers;
 
-public class ERC20CacheManager : DataDecoder
+public class ERC20CacheManager(ERC20CacheProvider erc20CacheProvider)
 {
-    private readonly ERC20CacheProvider _erc20CacheProvider;
-
-    public ERC20CacheManager(ERC20CacheProvider erc20CacheProvider)
-    {
-        _erc20CacheProvider = erc20CacheProvider;
-    }
-
-    public ERC20CacheManager()
-    {
-        var region = RegionEndpoint.GetBySystemName(Environments.AwsRegion.Get());
-        var client = new AmazonDynamoDBClient(region);
-        var context = new DynamoDBContext(client);
-        var erc20StorageProvider = new ERC20StorageProvider(context);
-        _erc20CacheProvider = new ERC20CacheProvider(erc20StorageProvider);
-    }
+    public ERC20CacheManager() : this(new ERC20CacheProvider()) { }
 
     public ERC20DynamoDbTable GetTokenInfo(long chainId, string tokenAddress)
     {
         var cacheRequest = new ApiRequestFactory().CreateCacheRequest(Environments.LastBlockKey.Get(), chainId, tokenAddress, Environments.ApiUrl.Get());
 
-        return _erc20CacheProvider.GetOrAdd(cacheRequest);
+        return erc20CacheProvider.GetOrAdd(cacheRequest);
     }
 }
