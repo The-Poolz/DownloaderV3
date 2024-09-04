0a8b33ff6f946bfd25cc0ffa4cfc69e1bb2a9c6e
Artem Krol <artemkrol47@gmail.com>
2024-09-04 10:00:46 -0500

    Reorgonize

--- test/DownloaderV2.Tests/DownloaderV2RunTests.cs
+++ test/DownloaderV2.Tests/DownloaderV2RunTests.cs
@@ -1,5 +1,6 @@
 ﻿using DownloaderContext;
 using DownloaderContext.Models;
+using DownloaderV2.Tests.Results.DbResults;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Moq;
@@ -16,21 +17,7 @@ namespace DownloaderV2.Tests
 
             var settings = new List<DownloaderSettings>
             {
-                new DownloaderSettings
-                {
-                    ChainId = 97,
-                    StartingBlock = 1,
-                    EndingBlock = 100,
-                    MaxBatchSize = 50000,
-                    Key = "myKey",
-                    ResponseType = "SwapBNBParty",
-                    ContractAddress = "0x1Fa306AAfcbf6B5e13a19c02E044ee28588B48ae",
-                    EventHash = "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67",
-                    Active = true,
-                    MaxPageNumber = 1000,
-                    UrlSet = "https://api.covalenthq.com/v1/[ChainId]/events/address/[ContractAddress]/?starting-block=[StartingBlock]&ending-block=[EndingBlock]&page-number=[PageNumber]&page-size=[MaxPageNumber]&key=[Key]"
-
-                }
+                DownloaderSettingsResult.SwapBNBPartyV1
             };
 
             var settingsMockSet = CreateMockDbSet(settings);
--- test/DownloaderV2.Tests/Results/DbResults/ChainResult.cs
+++ test/DownloaderV2.Tests/Results/DbResults/ChainResult.cs
@@ -4,25 +4,26 @@ namespace DownloaderV2.Tests.Results.DbResults;
 
 public class ChainResult
 {
-    public static readonly List<ChainInfo> ChainList = new()
-    {
+    public static readonly List<ChainInfo> ChainList =
+    [
         new ChainInfo
         {
             ChainId = 56,
             BlockPerSecond = 0.333f,
             SecondsToWarning = 1000,
-            SecondsToError  = 1000,
+            SecondsToError = 1000,
             DownloadTimeDelay = 60
         },
+
         new ChainInfo
         {
             ChainId = 1,
             BlockPerSecond = 0.067f,
             SecondsToWarning = 600,
-            SecondsToError  = 600,
+            SecondsToError = 600,
             DownloadTimeDelay = 60
         }
-    };
+    ];
 
     public static readonly Dictionary<long, ChainInfo> ChainSettings = new()
     {
