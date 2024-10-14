da948d84d33174fbb8bc7aff76b120bbbf05b592
Artem Krol <artemkrol47@gmail.com>
2024-10-14 09:35:06 -0500

    Updated added PropertySetterAttribute

---
+++ src/DownloaderV3.Destination/Attributes/PropertySetterAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace DownloaderV3.Destination.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class PropertySetterAttribute(string propertyName, string propertyValue, string dbSetName)
+    : Attribute
+{
+    public string PropertyName { get; } = propertyName;
+    public string PropertyValue { get; } = propertyValue;
+    public string DbSetName { get; } = dbSetName;
+}
